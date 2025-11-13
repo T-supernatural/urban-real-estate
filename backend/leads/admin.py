0789bfbbb2bac624aa187cb7b9de948e457f49c1
unknown <tinuademichael@gmail.com>
2025-11-13 01:30:29 -0800

    Add admin configurations for Lead and Listing models with inline support for ListingImage

--- backend/leads/admin.py
+++ backend/leads/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Lead
 
-# Register your models here.
+
+@admin.register(Lead)
+class LeadAdmin(admin.ModelAdmin):
+	list_display = ("name", "email", "phone_number", "property_interested_in", "created_at")
+	list_filter = ("created_at", "property_interested_in")
+	search_fields = ("name", "email", "phone_number")
+	readonly_fields = ("created_at",)
--- backend/listings/admin.py
+++ backend/listings/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Listing, ListingImage
 
-# Register your models here.
+
+class ListingImageInline(admin.TabularInline):
+	model = ListingImage
+	extra = 1
+
+
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+	list_display = (
+		"title",
+		"price",
+		"location",
+		"bedrooms",
+		"bathrooms",
+		"is_published",
+		"created_at",
+	)
+	list_filter = ("is_published", "created_at")
+	search_fields = ("title", "description", "location")
+	inlines = (ListingImageInline,)
+
+
+@admin.register(ListingImage)
+class ListingImageAdmin(admin.ModelAdmin):
+	list_display = ("listing", "caption", "uploaded_at")
+	search_fields = ("caption",)
+admin
